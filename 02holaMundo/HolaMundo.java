dd51aac056617b4087bfd7a2e3bfd511f5c9d3b4
demonassit <jminorg@ipn.mx>
2022-09-19 11:01:34 -0500

    hola mundo

---
+++ 02holaMundo/HolaMundo.java
@@ -0,0 +1,20 @@
+//esto es un comentario
+/*
+ * este comentario nos sirve para poder tener varias lineas comentadas del codigo
+ * 
+ * Todo programa en java, maneja clases, una clase es un objeto del mundo real
+ */
+
+ class HolaMundo{
+    //cuerpo del programa
+    /*
+     * aqui es donde nosotros codificamos todos los elementos del programa, funciones, estructuras, variables y metodos
+     * toda clase debe de tener un metodo principal
+     */
+
+     public static void main(String[] args){
+        System.out.println("Hola Mundo");
+     }
+
+ }
+
