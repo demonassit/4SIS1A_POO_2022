33b09541c067e16d0f6add9f6ba05c24c66414aa
demonassit <jminorg@ipn.mx>
2022-09-23 09:24:39 -0500

    para que no vienen

---
+++ 04EstructurasDatos/EstructuraDatos.java
@@ -0,0 +1,58 @@
+/**Vamos a crear los 14 programas dentro de un menu de seleccion de la tarea, comprendido por:
+ * 1.- Bono o descuento por edad
+ * 2.- Convertir numeros decimales a binarios
+ * 3.- convertir temperaturas
+ * 4.- Numero de positivos y negativos
+ * 5.- Tienda
+ * 6.- Area y Perimetro
+ * 7.- Tabla
+ * 8.- Factorial
+ * 9.- Dibujitos
+ * 10.- Figura hueca
+ * 11.- Patrones
+ * 12.- Diamante
+ * 13.- Calculadora
+ * 14.- Salir
+ */
+
+ import java.util.Scanner;
+
+class EstructuraDatos {
+
+    public static void main(String[] args){
+        //objetos
+        Scanner entrada = new Scanner(System.in);
+
+        //aqui van a ir las variables
+        int opcion, numbinario;
+        char letra;
+        String binario = "";
+
+        do{
+            System.out.println("Bienvenido al programa de las tareas que son para el lunes a las 7 am wiii (n_n)/");
+            System.out.println("Por favor elija la opcion deseada: ");
+            System.out.println("1.- Descuento por edad ");
+            System.out.println("2.- Convertir numero decimal a binario ");
+            System.out.println("3.- Conversiones de temperatura ");
+            System.out.println("4.- Numeros Positivos y Negativos ");
+            System.out.println("5.- Tienda ");
+            System.out.println("6.- Area y Perimetro ");
+            System.out.println("7.- Tabla ");
+            System.out.println("8.- Factorial ");
+            System.out.println("9.- Dibujos de codigo ");
+            System.out.println("10.- Figura Hueca ");
+            System.out.println("11.- Patrones de Codigo ");
+            System.out.println("12.- Diamante ");
+            System.out.println("13.- Calculadora ");
+            System.out.println("14.- Salir ");
+
+            //obtener la respuesta del usuario
+            opcion = entrada.nextInt();
+
+
+
+        }while(false);
+
+    }
+    
+}
