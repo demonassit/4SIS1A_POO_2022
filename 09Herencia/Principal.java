610d201f9c4a3949e28a36df5a08188c4a81e5e1
demonassit <jminorg@ipn.mx>
2022-10-17 10:50:00 -0500

    tarea de animalitos

--- 09Herencia/Principal.java
+++ 09Herencia/Principal.java
@@ -6,6 +6,19 @@ public class Principal {
         Perro dog = new Perro("Pulgas", "de la calle", "Croquetas", 1, "gua gua");
         Gato cat = new Gato("Michi", "de la calle", "Atun", 1, 7);
 
+        /*Quiero que se cree un menu mediante el cual el usuario pueda ingresar los datos de los animales:
+         * 
+         * 1.- Perro
+         * 2.- Gato
+         * 3.- Hamster
+         * 4.- Conejo
+         * 5.- Cuyo
+         * 6.- Uron
+         * 7.- Otro
+         * 
+         * Debera de poder ingresar cada uno de los datos y asi mismo deberan de entregar el diagrama de clases hecho en star uml aplicando la herencia correspondiente y el programa
+        */
+
         //muestro
         dog.mostrarPerro();
         cat.mostrarGato();
