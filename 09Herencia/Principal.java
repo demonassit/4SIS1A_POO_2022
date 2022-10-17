d046ba90a9ce6029bceabda013eafcaf4541e43a
demonassit <jminorg@ipn.mx>
2022-10-17 10:35:41 -0500

    principal

---
+++ 09Herencia/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+    public static void main(String[] args){
+        //hago la instancia del animal perro
+        Perro dog = new Perro("Pulgas", "de la calle", "Croquetas", 1, "gua gua");
+
+        //muestro
+        dog.mostrarPerro();
+    }
+    
+}
